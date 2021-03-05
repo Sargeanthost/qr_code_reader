0f4f4fc7636c9116dbe78eaf1bc8a8adb01eb27b
Sargeanthost <36427968+Sargeanthost@users.noreply.github.com>
2021-03-04 20:28:30 -0800

    style and refactor + new bug

--- qrcodereader.py
+++ qrcodereader.py
@@ -18,6 +18,7 @@ windowName = "Qr Reader"
 iconFolder = Path.joinpath(Path(__file__).resolve().parent, "icons")
 links = []
 
+
 def scaledNumber(newMin, newMax, minimum, maximum, toScale):
     """Inversely scale toMap to between newMin and newMax
 
@@ -31,9 +32,8 @@ def scaledNumber(newMin, newMax, minimum, maximum, toScale):
     Returns:
         float: Scaled number
     """
-    newNumber = (
-        ((newMin - newMax) * (float(toScale) - prevMin)) / (prevMax - prevMin)
-    ) + newMax
+    newNumber = (((newMin - newMax) * (float(toScale) - minimum)) /
+                 (maximum - minimum)) + newMax
     if newNumber < newMin:
         return newMin
     elif newNumber > newMax:
@@ -51,25 +51,28 @@ while True:
     for qrCode in qrCodes:
         message = qrCode.data.decode("utf-8")
 
-        # Currently only able to click the same website once per program instantiation. 
+        # Currently only able to click the same website once per program instantiation.
         if "http" in message:
             if message not in links:
                 links.append(message)
 
                 # On Windows .ico is required, on Linux - .png
-                strippedMessage = re.sub("[/\\:*?'\"<>|.]", "", message) + "favicon.ico"
-                with open(Path.joinpath(iconFolder, strippedMessage), "wb") as f:
+                strippedMessage = re.sub("[/\\:*?'\"<>|.]", "",
+                                         message) + "favicon.ico"
+                with open(Path.joinpath(iconFolder, strippedMessage),
+                          "wb") as f:
                     f.write(requests.get(message + "/favicon.ico").content)
 
                 pyn(
                     title="QR Code Link",
                     description=message,
-                    icon_path=str(Path.joinpath(iconFolder,strippedMessage)), # On Windows .ico is required, on Linux - .png
+                    icon_path=str(
+                        Path.joinpath(iconFolder, strippedMessage)
+                    ),  # On Windows .ico is required, on Linux - .png
                     duration=10,
                     urgency=pyn.URGENCY_CRITICAL,
                     callback_on_click=lambda: webbrowser.open(
-                        message, new=0, autoraise=True
-                    ),
+                        message, new=0, autoraise=True),
                 ).send()
 
         corners = np.array(qrCode.polygon, np.int32)
@@ -91,18 +94,20 @@ while True:
             3,
         )
 
-        cv2.putText(frame, message, (cornersFlat[0], cornersFlat[1]-20), font, fontScale,(255, 240, 240), 3)      
-    
+        cv2.putText(frame, message, (cornersFlat[0], cornersFlat[1] - 20), font,
+                    fontScale, (255, 240, 240), 3)
+
     cv2.imshow(windowName, frame)
     keyCode = cv2.waitKey(1)
-    
-    if cv2.getWindowProperty(windowName, cv2.WND_PROP_VISIBLE) < 1 or keyCode == 27:
+
+    if cv2.getWindowProperty(windowName,
+                             cv2.WND_PROP_VISIBLE) < 1 or keyCode == 27:
         break
 
-#delete downloaded favicons
+#delete downloaded favicons on close
 for files in os.listdir(str(iconFolder)):
     if files.endswith(".ico") or files.endswith(".png"):
-        os.remove(str(Path.joinpath(iconFolder,files)))
+        os.remove(str(Path.joinpath(iconFolder, files)))
     else:
         continue
 
